5985f13d17a46dd449769c6bb2cd322beaa42cd0
Michael Weiner <6993102@isd535.org>
2019-04-13 18:33:34 -0500

    Add template overrides for standard and pro versions of The Events Calednar & for events created with the block editor.

--- plugin.php
+++ plugin.php
@@ -17,16 +17,34 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 // Call 'trive_filter_template_paths' to add an additional directory to look for template overrides
 function tribe_filter_template_paths ( $file, $template ) {
-	// Set the path for the event system to look for additional overrides
-	$custom_file_path = ABSPATH . 'wp-content/plugins/tribe-ext-venue-tooptip/tribe-events/' . $template;
+
+	// Set the path for the event system to look for additional overrides for events created with the classic editor for the standard and pro version of The Events Calendar
+	$custom_file_path = ABSPATH . 'wp-content/plugins/Tribe-Extension-Venue-On-Tooltip/tribe-events/' . $template;
+	$custom_file_path_pro = ABSPATH . 'wp-content/plugins/Tribe-Extension-Venue-On-Tooltip/tribe-events/pro/' . $template;
+
+	// Set the path for the event system to look for additional overrides for events created with the block editor for the standard and pro version of The Events Calendar
+	$custom_file_path_block = ABSPATH . 'wp-content/plugins/Tribe-Extension-Venue-On-Tooltip/tribe/events/' . $template;
+	$custom_file_path_block_pro = ABSPATH . 'wp-content/plugins/Tribe-Extension-Venue-On-Tooltip/tribe/events/pro/' . $template;
 
 	// If the event system does not find any template overrides in the directory specified --> use the default template files
 	if ( !file_exists($custom_file_path) ) {
 		return $file; // Return the original file
 	}
+	if ( !file_exists($custom_file_path_pro) ) {
+		return $file; // Return the original file
+	}
+	if ( !file_exists($custom_file_path_block) ) {
+		return $file; // Return the original file
+	}
+	if ( !file_exists($custom_file_path_block_pro) ) {
+		return $file; // Return the original file
+	}
 
-	// If the event system does find any template overrides in the directory specified --> use the new template override
+	// If the event system does find any template overrides in any of the directories specified --> use the new template override
 	return $custom_file_path;
+	return $custom_file_path_pro;
+	return $custom_file_path_block;
+	return $custom_file_path_block_pro;
 }
 
 add_filter( 'tribe_events_template', 'tribe_filter_template_paths', 10, 2 ); // Call the function created to check for additional overrides
