cfa3d12c183f9ab3f07a7291e4e18a4291348430
Michael Weiner <47571709+mike-weiner@users.noreply.github.com>
2019-04-29 20:49:25 -0500

    PUBLISHED - v. 1.0.1

--- plugin.php
+++ plugin.php
@@ -1,53 +1,83 @@
-<?php
-/**
- * Plugin Name: Display Event Locations for The Events Calendar
- * Plugin URI: https://thetechsurge.com/
- * Description: Add the event venue/location to the tooltip that is displayed on hover over in the month view of the claendar when using The Events Calenar or The Events Calendar Pro by Modern Tribe.
- * Author: Michael Weiner
- * Author URI: https://thetechsurge.com/
- * Version: 1.0.0
- * License: GPL2+
- * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
- */
+=== Display Event Location for The Events Calendar ===
+Contributors: vikings412
+Donate Link: https://paypal.me/michaelw13?locale.x=en_US
+Tags: events, customization, modern-tribe, override, template
+Requires at least: 4.6
+Tested up to: 5.1.1
+Stable tag: 1.0.1
+Requires PHP: 5.4
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
-// Exit plugin if it is being accessed directly
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+This plugin works with The Events Calendar by Modern Tribe. It changes the popup on the month view to include the event's location within the popup.
 
-// Establish directory paths for the template overrides to be used the The Events Calendar Plugin already installed separately by the user
-// deltec_ is the custom prefix used for all classes and functions within the plugiin
-// Call 'deltec_trive_filter_template_paths' to add additional directory paths to look for template overrides
-// tribe_events_template() comes from the Modern Tribe The Events Claendar Plugin installled on the site -- this cannot be changed
-function deltec_tribe_filter_template_paths ( $file, $template ) {
+== Description ==
 
-	// Set the path for the event system to look for additional overrides for events created with the classic editor for the standard and pro version of The Events Calendar
-	$deltec_custom_file_path = plugin_dir_path( __FILE__ ) . 'tribe-events/' . $template;
-	$deltec_custom_file_path_pro = plugin_dir_path( __FILE__ ) . 'tribe-events/' . $template;
+This plugin overrides the standard template for the tooltip that is displayed when a user hovers over an event on the month display of The Events Calendar. The name of the event's venue/location that is entered with the event will be displayed under the date and time that the event occurs. All of the other elements of the template remain unchanged.
 
-	// Set the path for the event system to look for additional overrides for events created with the block editor for the standard and pro version of The Events Calendar
-	$deltec_custom_file_path_block = plugin_dir_path( __FILE__ ) . 'tribe/events/' . $template;
-	$deltec_custom_file_path_block_pro = plugin_dir_path( __FILE__ ) . 'tribe/events/' . $template;
+== Installation ==
 
-	// If the event system does not find any template overrides in the directory specified --> return the default template files
-	if ( !file_exists($deltec_custom_file_path) ) {
-		return $file;
-	}
-	if ( !file_exists($deltec_custom_file_path_pro) ) {
-		return $file;
-	}
-	if ( !file_exists($deltec_custom_file_path_block) ) {
-		return $file;
-	}
-	if ( !file_exists($deltec_custom_file_path_block_pro) ) {
-		return $file;
-	}
+This section describes how to install the plugin and get it working.
 
-	// If the event system does find any template overrides in any of the directories specified --> return the new template override
-	return $deltec_custom_file_path;
-	return $deltec_custom_file_path_pro;
-	return $deltec_custom_file_path_block;
-	return $deltec_custom_file_path_block_pro;
-}
+1. Upload the plugin files to the plugins directory, or install the plugin through the WordPress Plugins panel directly.
+1. Activate the plugin through the Plugins panel in the WordPress Admin Backend
+1. Disable 'The Events Calendar' or 'The Events Calendar Pro'
+1. Re-Enable 'The Events Calendar' or 'The Events Calendar Pro'
 
-add_filter( 'tribe_events_template', 'deltec_tribe_filter_template_paths', 10, 2 ); // Call the function created to check for additional overrides
+== Frequently Asked Questions ==
+
+= I have activated the plugin, but the tooltip still looks the same. Why does the tooltip not list the event's location? =
+
+Make sure that the plugin 'Display Event Locations for The Events Calendar' is enabled in the backend. Once it is enabled and active, deactivate the plugin for 'The Events Calendar' or 'The Events Calendar Pro' and then re-activate it. When you hover over an event on the month view, the tooltip should now display the event's location name.
+
+= Does this plugin display anything other than the name of the event's location? =
+
+No. At this time, this plugin only adds the name of the event's location/venue to the tooltip.
+
+= What plugins are required for these changes to take effect? =
+
+Your website must use 'The Events Calendar 4.8' or 'The Events Calendar Pro 4.8' or higher for this plugin to correctly modify the tooltip.
+
+= Does this plugin change the tooltip for both regular and featured events? =
+
+Yes, this plugin will change the tooltip for both regular and featured events.
+
+= Does this plugin work for events created with the block editor? =
+
+Yes. This plugin will modify the tooltip for events created with the classic/standard editor or the block editor for both the standard and pro versions of 'The Events Calendar'.
+
+== Screenshots==
+
+1. This is what the tooltip for a regular event will look like after activating this plugin.
+2. This is what the tooltip for a featured event will look like after activating this plugin.
+
+== Changelog ==
+
+= 1.0.1 =
+* FIXED - Adjusted readme.txt to display screenshots
+* CHANGED - Changed CSS Class from 'tribe-events-location' to 'deltec-tribe-events-location'
+* MISC. - Added Plugin Logo Assets
+
+= 1.0 =
+* Released on April 29, 2019
+* Initial release
+
+== Upgrade Notice ==
+
+= 1.0.1 =
+Fixed typos in readme.txt
+Added Logos
+Changed CSS Class from 'tribe-events-location' to 'deltec-tribe-events-location'
+
+= 1.0 =
+Initial release!
+
+== Arbitrary section ==
+
+=== References: ===
+1. https://theeventscalendar.com/support/forums/topic/altering-event-tooltip-display-displaying-additional-fields-in-tooltips/
+1. https://theeventscalendar.com/support/forums/topic/override-templates-via-custom-plugin/
+1. https://support.theeventscalendar.com/153124-Themers-Guide
+
+=== CSS Classes: ===
+* deltec-tribe-events-location - Within the tooltip/popup the text 'Location: Event-Location-Here' is wrapped in this class. Use this to style the information within the popup.
